6532c49678fd21f2809650b2bc9654e9b6395ce7
Valeria <valeriastetsiuk3546@gmail.com>
2022-10-23 14:29:21 +0300

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,31 @@
-// import { FaBeer } from 'react-icons/fa';
-// import ContentLoader, { Facebook } from 'react-content-loader'
-// import { ToastContainer, toast } from 'react-toastify';
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Gallery } from './ImageGallery/ImageGallery';
 
-export const App = () => {
-  return (
-    <>
-     
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    searchQuery: '',
+    gallery: [],
+    isLoading: false,
+    error: null,
+  };
+
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  render() {
+    const { searchQuery} = this.state
+   
+    return (
+      <>
+        <Searchbar
+          onSubmit={this.handleFormSubmit}
+        />
+        <Gallery
+          searchQuery={searchQuery}
+        />
+      </>
+    );
+  }
+}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -7,7 +7,6 @@ import { GalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Button } from '../Button/Button';
 import { Modal } from '../Modal/Modal';
 import ThreeDots from '../Loader/Loader';
-import s from './ImageGallery.module.css';
 
 
 
@@ -24,7 +23,88 @@ export class Gallery extends Component {
         searchQuery: PropTypes.string.isRequired,
         onUpdate: PropTypes.func.isRequired,
     }
+ async componentDidUpdate(prevProps, prevState) {
+    const prevQuery = prevProps.searchQuery;
+    const nextQuery = this.props.searchQuery;
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
 
+     if (prevQuery !== nextQuery) {
+      try {
+        this.setState({ isLoading: true, page: 1, gallery: [] });
+        const pictureData = await imageApi(nextQuery);
+        this.setState({ gallery: pictureData });  
+      } catch (err) {
+        this.setState({ error: err.message });
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+    if (prevPage !== nextPage && nextPage !== 1) {
+      try {
+        this.setState({ isLoading: true, error: '' });
+        const pictureData = await imageApi(
+          nextQuery,
+          nextPage
+        );
+          this.setState(({ gallery }) => ({
+          gallery: [...gallery, ...pictureData],
+        }));
+      } catch (err) {
+        this.setState({ error: err.message });
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+
+    if (!this.state.isLoading && this.state.gallery.length === 0) {
+      this.notify();
+    }
+  }
+   pagination = e => {
+    e.preventDefault();
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+  updateCurrentImage = data => {
+    this.setState({ currentImage: data });
+  };
+  closeModal = () => {
+    this.setState({ currentImage: null });
+  };
+
+  notify = () => {
+          toast.warn('Did not find anything! Please change the request.');
+  }
+
+  render() {
+    const { page, gallery, currentImage, isLoading, error } = this.state;
+
+    return (
+      <>
+        {error && (
+          <span className="error">Oops! Something went wrong. {error}</span>
+        )}
+        {!isLoading && !error && gallery.length === 0 && <ToastContainer /> }
+        <ul className="imageGallery">
+          {!!gallery.length && (
+            <GalleryItem
+              gallery={gallery}
+              openModal={this.updateCurrentImage}
+            />
+          )}
+        </ul>
+        {isLoading && <ThreeDots />}
+        {!!gallery.length &&
+          gallery.length >= page * 12 && (
+            <Button pagination={this.pagination} />
+          )}
+
+        {currentImage && (
+          <Modal image={currentImage} closeModal={this.closeModal} />
+        )}
+      </>
+    );
+  }
+}
 
     
-}
