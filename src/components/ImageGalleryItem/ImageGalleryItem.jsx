8b9406198d1c2494e1490dd8f3400a0876a9003a
Valeria <valeriastetsiuk3546@gmail.com>
2022-10-23 15:19:12 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,21 +1,21 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 
-// export const GalleryItem = ({ gallery, openModal }) => {
-//   return (
-//     <>
-//       {gallery.map(({ id, webformatURL, tags, largeImageURL }) => (
-//         <li key={id} className="imageGalleryItem">
-//           <a href="#" onClick={() => {openModal({ src: largeImageURL, alt: tags }); }} rel="noreferrer" >
-//             <img className="imageGalleryItem-image" src={webformatURL} alt={tags} width="350"  />
-//           </a>
-//         </li>
-//       ))}
-//     </>
-//   );
-// };
+export const GalleryItem = ({ gallery, openModal }) => {
+  return (
+    <>
+      {gallery.map(({ id, webformatURL, tags, largeImageURL }) => (
+        <li key={id} className="imageGalleryItem">
+          <a href="#" onClick={() => {openModal({ src: largeImageURL, alt: tags }); }} rel="noreferrer" >
+            <img className="imageGalleryItem-image" src={webformatURL} alt={tags} width="350"  />
+          </a>
+        </li>
+      ))}
+    </>
+  );
+};
 
-// GalleryItem.propTypes={
-//   gallery: PropTypes.array.isRequired,
-//   openModal: PropTypes.func.isRequired,
-// }
+GalleryItem.propTypes={
+  gallery: PropTypes.array.isRequired,
+  openModal: PropTypes.func.isRequired,
+}
